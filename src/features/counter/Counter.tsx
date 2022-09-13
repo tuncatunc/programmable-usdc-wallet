b240e955da7993c0ea0feb25a777152c22669b04
tuncatunc <atuncatunc@gmail.com>
2022-09-13 06:34:16 +0300

    dev

---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import type { RootState } from "../../app/store"
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement } from "./counterSlice"
+
+export const Counter = () => {
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return
+  <div className="h-full w-32">
+    <button className="btn btn-blue"
+      onClick={dispatch(increment)}
+    >
+      Increment
+    </button>
+  </div>
+}
