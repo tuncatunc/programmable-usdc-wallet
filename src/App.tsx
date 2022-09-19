163836e507f9fa13eb87a16bc00f5ab1bfdff426
tuncatunc <atuncatunc@gmail.com>
2022-09-19 20:11:39 +0300

    adding wallet dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { FC, useMemo } from 'react';
 import { Provider } from 'react-redux';
 import { StyledEngineProvider } from '@mui/material/styles';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { FakeWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+  WalletDialogProvider,
+  WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-material-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
 import './App.css';
 import { AppRoutes } from "./routes"
 import { store } from "./app/store";
 
 function App() {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      /**
+       * Select the wallets you wish to support, by instantiating wallet adapters here.
+       *
+       * Common adapters can be found in the npm package `@solana/wallet-adapter-wallets`.
+       * That package supports tree shaking and lazy loading -- only the wallets you import
+       * will be compiled into your application, and only the dependencies of wallets that
+       * your users connect to will be loaded.
+       */
+      new FakeWalletAdapter(),
+    ],
+    []
+  );
+
   return (
-    <Provider store={store}>
-      <StyledEngineProvider injectFirst>
-        <AppRoutes />
-      </StyledEngineProvider>
-    </ Provider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletDialogProvider>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+          <Provider store={store}>
+            <StyledEngineProvider injectFirst>
+              <AppRoutes />
+            </StyledEngineProvider>
+          </ Provider>
+          { /* Your app's components go here, nested within the context providers. */}
+        </WalletDialogProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
