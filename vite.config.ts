6d21f6f35ad8422cad1c398e85345eba9f759380
tuncatunc <atuncatunc@gmail.com>
2022-09-25 15:04:11 +0300

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    esbuildOptions: { target: "es2020", supported: { bigint: true } }
+    esbuildOptions: { 
+      target: "es2020", 
+      supported: { bigint: true },
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+    },
+    // Enable esbuild polyfill plugins
+    plugins: [
+        NodeGlobalsPolyfillPlugin({
+            buffer: true
+        })
+    ]
+    }
   },
   define: {
     'process.env': {},
@@ -13,7 +26,7 @@ export default defineConfig({
 
   },
   build: {
-    target: [ 'es2020' ],
+    target: ['es2020'],
     rollupOptions: {
       plugins: []
     }
