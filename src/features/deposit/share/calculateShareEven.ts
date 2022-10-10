597a50e896ac4f5b66f21308c538d4591b2367fe
tuncatunc <atuncatunc@gmail.com>
2022-10-11 00:34:29 +0300

    fix deposit balance

--- src/features/deposit/share/calculateShareEven.ts
+++ src/features/deposit/share/calculateShareEven.ts
@@ -1,5 +1,6 @@
+import { decimals } from "../../../common/usdcMint"
 import { IPortfolio } from "../../portfolio/portfolio"
 
-export const calculateShareEven = (portfolio: IPortfolio, amount: number): number[] => {
-  return portfolio.subaccounts.map(sa => Math.floor(amount / portfolio.subaccounts.length))
+export const calculateShareEven = (portfolio: IPortfolio, amount: number): bigint[] => {
+  return portfolio.subaccounts.map(sa => BigInt(amount) / BigInt(portfolio.subaccounts.length) * decimals)
 }
